f5e7704d7ecdaf68335dae09725a008d8b46a917
ztycn <zhaotianyuecn@live.com>
2021-08-25 23:53:14 +0800

    feat: create a http web app via golang

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
